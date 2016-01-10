3e9e0e11949e724d6ab190165011d7254d23b116
rajin9601 <rajin9601@naver.com>
2016-01-10 16:15:49 +0900

    Implement delete in my lecture list

--- SNUTT/STMyLectureListController.swift
+++ SNUTT/STMyLectureListController.swift
@@ -49,25 +49,20 @@ class STMyLectureListController: UITableViewController {
     }
     
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
-    // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
-            // Delete the row from the data source
+            STTimetableManager.sharedInstance.currentTimetable?.deleteLecture((STTimetableManager.sharedInstance.currentTimetable?.lectureList[indexPath.row])!)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
