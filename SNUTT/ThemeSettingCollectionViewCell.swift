98307a9ccc5db11870cee25ee21e3d1bc91b7e7d
JSKeum <jskeum2512@gmail.com>
2021-08-02 16:54:26 +0900

    fix selected theme color

--- SNUTT/ThemeSettingCollectionViewCell.swift
+++ SNUTT/ThemeSettingCollectionViewCell.swift
@@ -11,7 +11,12 @@ import UIKit
 class ThemeSettingCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var themeImage: UIImageView!
-    @IBOutlet weak var themeLabel: UILabel!
+    @IBOutlet weak var themeLabel: UILabel! {
+        didSet {
+            themeLabel.layer.cornerRadius = 10
+            themeLabel.clipsToBounds = true
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -24,5 +29,13 @@ class ThemeSettingCollectionViewCell: UICollectionViewCell {
     
     func setLabelText(_ text: String) {
         themeLabel.text = text
+    }
+    
+    func setThemeSelected() {
+        themeLabel.backgroundColor = UIColor(hexString: "#F2F2F2")
+    }
+    
+    func setThemeDeselected() {
+        themeLabel.backgroundColor = nil
     }
 }
