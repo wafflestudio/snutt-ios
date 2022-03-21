df5eaba4fc01db752336f95c4d0ce84435b13554
shinhong_park <shp7724@snu.ac.kr>
2022-03-21 22:18:13 +0900

    fix: add polyfills for iOS 13

--- SNUTT-2022/SNUTT/Views/SNUTTApp.swift
+++ SNUTT-2022/SNUTT/Views/SNUTTApp.swift
@@ -7,7 +7,21 @@
 
 import SwiftUI
 
+/// Wrapper struct that supports both iOS 13 and iOS 14 (and above).
 @main
+struct SNUTTAppWrapper {
+    static func main() {
+        if #available(iOS 14.0, *) {
+            SNUTTApp.main()
+        }
+        else {
+            /// In iOS 13, use `SceneDelegate` instead of `@main`.
+            UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, NSStringFromClass(SceneDelegate.self))
+        }
+    }
+}
+
+@available(iOS 14.0, *)
 struct SNUTTApp: App {
     var body: some Scene {
         WindowGroup {
---
+++ SNUTT-2022/SNUTT/Views/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  SNUTT
+//
+//  Created by 박신홍 on 2022/03/21.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+/// Polyfill for iOS 13.
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let contentView = ContentView()
+
+        // Use a UIHostingController as window root view controller.
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+}
