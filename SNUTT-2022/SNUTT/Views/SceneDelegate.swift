ccd739876a8fe5edf151085463178ea7e1391589
shinhong_park <shp7724@snu.ac.kr>
2022-03-22 02:24:35 +0900

    fix: xcode 11, iOS 13 @main error

--- SNUTT-2022/SNUTT/Views/SceneDelegate.swift
+++ SNUTT-2022/SNUTT/Views/SceneDelegate.swift
@@ -9,19 +9,3 @@ import Foundation
 import SwiftUI
 import UIKit
 
-/// Polyfill for iOS 13.
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    var window: UIWindow?
-
-    func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
-        let contentView = ContentView()
-
-        // Use a UIHostingController as window root view controller.
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
-            self.window = window
-            window.makeKeyAndVisible()
-        }
-    }
-}
