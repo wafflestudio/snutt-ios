911c4e2c248a1b860bb3414df59d7830bc40dfa1
shinhong_park <shp7724@snu.ac.kr>
2022-04-03 01:40:18 +0900

    feat: support more precise end period

--- SNUTT/SNUTT/STTime.swift
+++ SNUTT/SNUTT/STTime.swift
@@ -15,19 +15,31 @@ struct STTime {
     var endPeriod: Double {
         return startPeriod + duration
     }
+    
+    /// 수업이 실제로 끝나는 시각(교시). `duration`이 `0.5`로 끝나는지에 따라 적절한 시간(10분 또는 15분)을 빼준다.
+    var endPeriodPrecise: Double {
+        let isHalfPeriod = duration.truncatingRemainder(dividingBy: 1) == 0.5
+        return startPeriod + duration - (isHalfPeriod ? 15/60 : 10/60)
+    }
 
     init(day: Int, startPeriod: Double, duration: Double) {
         self.day = STDay(rawValue: day)!
         self.startPeriod = startPeriod
         self.duration = duration
     }
+    
+    
 
     func longString() -> String {
-        return day.longString() + " " + startPeriod.periodString() + "~" + endPeriod.periodString()
+        let ret =  day.longString() + " " + startPeriod.periodString() + "~" + endPeriodPrecise.periodStringPrecise()
+        print("long", ret);
+        return ret
     }
 
     func shortString() -> String {
-        return day.shortString() + " " + startPeriod.periodString() + "~" + endPeriod.periodString()
+        let ret =  day.shortString() + " " + startPeriod.periodString() + "~" + endPeriodPrecise.periodStringPrecise()
+        print("short", ret)
+        return ret
     }
 
     func startString() -> String {
