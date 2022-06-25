9dc9fd1ccae9f0d0f53eb86e1df057f0f3b0f821
Park Shin Hong <shp7724@naver.com>
2022-06-25 09:52:00 +0900

    fix: create ContentView and refactor tab views with TabScene wrapper

---
+++ SNUTT-2022/SNUTT/Views/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  SNUTT
+//
+//  Created by 박신홍 on 2022/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject var systemState = AppState.of.system
+    
+    var body: some View {
+        TabView(selection: $systemState.selectedTab) {
+            TabScene(tabType: .timetable) {
+                MyTimetableScene()
+            }
+            TabScene(tabType: .search) {
+                MyLectureListScene()
+            }
+            TabScene(tabType: .review) {
+                ReviewScene()
+            }
+            TabScene(tabType: .settings) {
+                SettingScene()
+            }
+        }
+        .accentColor(Color(UIColor.label))
+        .onAppear {
+            setTabBarStyle()
+            setNavBarStyle()
+        }
+        let _ = debugChanges()
+    }
+    
+    /// Globally set the background color of the tab bar to white.
+    private func setTabBarStyle() {
+        let appearance = UITabBarAppearance()
+        appearance.backgroundColor = .white
+        UITabBar.appearance().standardAppearance = appearance
+        if #available(iOS 15.0, *) {
+            UITabBar.appearance().scrollEdgeAppearance = appearance
+        }
+    }
+    
+    /// Globally set the background color of the nav bar to white.
+    private func setNavBarStyle() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .white
+        UINavigationBar.appearance().standardAppearance = appearance
+        if #available(iOS 15.0, *) {
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SNUTT-2022/SNUTT/Views/Scenes/TabScene.swift
@@ -0,0 +1,41 @@
+//
+//  TabScene.swift
+//  SNUTT
+//
+//  Created by 박신홍 on 2022/06/25.
+//
+
+import SwiftUI
+
+struct TabScene<Content>: View where Content: View {
+    var systemState = AppState.of.system
+    let tabType: AppState.TabType
+    @State var isViewDisplayed = false
+    @ViewBuilder var content: () -> Content
+    
+    var onImageName: String {
+        "tab.\(tabType.rawValue).on"
+    }
+
+    var offImageName: String {
+        "tab.\(tabType.rawValue).off"
+    }
+    
+    var body: some View {
+        NavigationView {
+            self.content()
+        }
+        .accentColor(Color(UIColor.label))
+        .tabItem {
+            Image(self.isViewDisplayed ? onImageName : offImageName)
+        }
+        .onAppear {
+            self.isViewDisplayed = true
+        }
+        .onDisappear {
+            self.isViewDisplayed = false
+        }
+        .tag(tabType)
+    }
+}
+
