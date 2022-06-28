a1418102b6925f44c255e9cf56f91a6c99173b6c
Park Shin Hong <shp7724@naver.com>
2022-06-28 23:10:13 +0900

    refact: separate LectureBlocks from TimetableBlocksLayer

--- SNUTT-2022/SNUTT/Views/Components/Timetable/LectureBlocks.swift
+++ SNUTT-2022/SNUTT/Views/Components/Timetable/LectureBlocks.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct LectureBlocks: View {
+    typealias Painter = TimetableViewModel.TimetablePainter
+    
+    let lecture: Lecture
+    @EnvironmentObject var timetableSetting: TimetableSetting
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { reader in
+            ForEach(lecture.timePlaces) { timePlace in
+                if let offsetPoint = Painter.getOffset(of: timePlace, in: reader.size, timetableSetting: timetableSetting) {
+                    TimetableBlock(lecture: lecture, timePlace: timePlace)
+                        .frame(width: Painter.getWeekWidth(in: reader.size, weekCount: timetableSetting.weekCount), height: Painter.getHeight(of: timePlace, in: reader.size, hourCount: timetableSetting.hourCount), alignment: .center)
+                        .offset(x: offsetPoint.x, y: offsetPoint.y)
+                }
+            }
+        }
     }
 }
 
 struct LectureBlocks_Previews: PreviewProvider {
     static var previews: some View {
-        LectureBlocks()
+        let viewModel = TimetableViewModel(appState: AppState())
+        LectureBlocks(lecture: viewModel.currentTimetable.lectures[0])
+            .environmentObject(viewModel.timetableSetting)
     }
 }
--- SNUTT-2022/SNUTT/Views/Components/Timetable/TimetableBlocksLayer.swift
+++ SNUTT-2022/SNUTT/Views/Components/Timetable/TimetableBlocksLayer.swift
@@ -8,23 +8,13 @@
 import SwiftUI
 
 struct TimetableBlocksLayer: View {
-    typealias Painter = TimetableViewModel.TimetablePainter
-    @EnvironmentObject var timetableSetting: TimetableSetting
     @EnvironmentObject var currentTimetable: Timetable
-
+    
     var body: some View {
-        GeometryReader { reader in
-            ForEach(currentTimetable.lectures) { lecture in
-                ForEach(lecture.timePlaces) { timePlace in
-                    if let offsetPoint = Painter.getOffset(of: timePlace, in: reader.size, timetableSetting: timetableSetting) {
-                        TimetableBlock(lecture: lecture, timePlace: timePlace)
-                            .frame(width: Painter.getWeekWidth(in: reader.size, weekCount: timetableSetting.weekCount), height: Painter.getHeight(of: timePlace, in: reader.size, hourCount: timetableSetting.hourCount), alignment: .center)
-                            .offset(x: offsetPoint.x, y: offsetPoint.y)
-                    }
-                }
-            }
+        ForEach(currentTimetable.lectures) { lecture in
+            LectureBlocks(lecture: lecture)
         }
-
+        
         let _ = debugChanges()
     }
 }
@@ -35,7 +25,7 @@ struct TimetableBlocks_Previews: PreviewProvider {
             let viewModel = TimetableViewModel(appState: AppState())
             TimetableBlocksLayer()
                 .environmentObject(viewModel.currentTimetable)
-                .environmentObject(viewModel.drawingSetting)
+                .environmentObject(viewModel.timetableSetting)
             TimetableGridLayer()
         }
     }
