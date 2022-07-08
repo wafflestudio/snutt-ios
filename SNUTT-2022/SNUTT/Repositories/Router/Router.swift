6892a186b731bb52dd70b050799168b6ed04092c
JSKeum <jskeum2512@gmail.com>
2022-07-08 17:29:53 +0900

    feat: add routers

---
+++ SNUTT-2022/SNUTT/Repositories/Router/Router.swift
@@ -0,0 +1,35 @@
+//
+//  Router.swift
+//  SNUTT
+//
+//  Created by Jinsup Keum on 2022/07/08.
+//
+
+import Foundation
+import Alamofire
+
+protocol Router: URLRequestConvertible {
+    var baseURL: URL { get }
+    var method: HTTPMethod { get }
+    var path: String { get }
+    var parameters: Parameters? { get }
+    
+    func asURLRequest() throws -> URLRequest
+}
+
+extension Router {
+    func asURLRequest() throws -> URLRequest {
+        let url = baseURL.appendingPathComponent(path)
+        var request = URLRequest(url: url)
+        request.method = method
+        
+        switch method {
+        case .get:
+            request = try URLEncoding.default.encode(request, with: parameters)
+        default:
+            request = try JSONEncoding.default.encode(request, with: parameters)
+        }
+        
+        return request
+    }
+}
