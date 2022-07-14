a10f5bd60263d11272258e4c4220f1aea2b168f5
peng-u-0807 <penguin0807@snu.ac.kr>
2022-07-15 01:50:29 +0900

    fix(orig): fix loading popup logic

--- SNUTT/SNUTT/AppDelegate.swift
+++ SNUTT/SNUTT/AppDelegate.swift
@@ -192,7 +192,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_: UIApplication) {
-        STPopupManager.initialize()
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
--- SNUTT/SNUTT/STPopupManager.swift
+++ SNUTT/SNUTT/STPopupManager.swift
@@ -18,22 +18,16 @@ struct STPopupManager {
     /// 유저에게 보여주지 않을 팝업까지 포함된 전체 리스트입니다.
     private static var totalPopupList: [STPopup] = []
 
-    /// UserDefaults에서 불러온 팝업
-    private static var local: [STPopup] = []
-
     /// 서버에서 받아온 팝업
-    private static var server: [STPopup] = []
+    private static var serverPopupList: [STPopup] = []
 
     static var hasShownPopup: Bool = false
 
     /// STPopupManager을 초기화합니다.
     static func initialize() {
         STPopupManager.hasShownPopup = false
-        STPopupManager.loadData()
-        STPopupManager.getRecentPopup(then: {
-            STPopupManager.totalPopupList = findOverlappingPopup(local: local, server: server)
-            saveData()
-        })
+        STPopupManager.getRecentPopup(then: loadData)
+        saveData()
     }
 
     /// 현재 popupList를 UserDefaults에 저장합니다.
@@ -58,46 +52,35 @@ struct STPopupManager {
     /// UserDefaults에 저장된 팝업 정보를 불러옵니다.
     private static func loadData() {
         if let dict = STDefaults[.popupList] {
-            local.removeAll()
             for data in dict {
                 guard let popup = STPopup(dictionary: data) else { continue }
                 #if DEBUG
-                    print(popup)
+                print(popup)
                 #endif
-                local.append(popup)
+                totalPopupList.indices.filter {
+                    totalPopupList[$0] == popup
+                }.forEach {
+                    totalPopupList[$0] = popup
+                }
             }
         }
     }
 
     /// 서버에서 최신 팝업 정보를 받아옵니다.
     private static func getRecentPopup(then: @escaping () -> Void) {
+        totalPopupList.removeAll()
         STNetworking.getPopups { popups in
             for popup in popups {
                 #if DEBUG
-                    print(popup)
+                print(popup)
                 #endif
-                if !server.contains(where: {
-                    $0 == popup
-                }) {
-                    server.append(popup)
-                }
+                totalPopupList.append(popup)
             }
             then()
             saveData()
         } failure: {}
     }
-
-    /// 로컬과 서버 모두에 존재하는 팝업만 반환합니다.
-    private static func findOverlappingPopup(local: [STPopup], server: [STPopup]) -> [STPopup] {
-        var result: [STPopup] = []
-        for localPopup in local {
-            if server.contains(localPopup) {
-                result.append(localPopup)
-            }
-        }
-        return result
-    }
-
+    
     /// 유저에게 보여야 하는 팝업만 남겨 반환합니다.
     private static func filterPopup() -> [STPopup] {
         return totalPopupList.filter { popup in
