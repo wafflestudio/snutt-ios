b9e2096c3915bfb60232ca42fad085708d9ff9a8
peng-u-0807 <peng-u-0807@users.noreply.github.com>
2022-09-17 06:54:36 +0000

    Apply SwiftFormat changes

--- SNUTT-2022/SNUTT/ViewModels/AccountSettingViewModel.swift
+++ SNUTT-2022/SNUTT/ViewModels/AccountSettingViewModel.swift
@@ -9,13 +9,12 @@ import Combine
 import SwiftUI
 
 class AccountSettingViewModel: BaseViewModel, ObservableObject {
-    
     @Published private var _currentUser: User?
     private var bag = Set<AnyCancellable>()
-    
+
     override init(container: DIContainer) {
         super.init(container: container)
-        
+
         // TODO: change this
         appState.user.$current
             .compactMap { $0 }
@@ -24,27 +23,28 @@ class AccountSettingViewModel: BaseViewModel, ObservableObject {
             }
             .store(in: &bag)
     }
-    
+
     // TODO: implement destinations of each menu
     var menuList: [[SettingsMenu]] {
         var menu: [[SettingsMenu]] = []
         menu.append(currentUser?.localId == nil
-                    ? [SettingsMenu(AccountSettings.addLocalId) {
-                        AddLocalIdScene(viewModel: AddLocalIdViewModel(container: container))}]
-                    : [SettingsMenu(AccountSettings.showLocalId,
-                                    content: currentUser?.localId ?? "(없음)"),
-                       SettingsMenu(AccountSettings.changePassword)])
+            ? [SettingsMenu(AccountSettings.addLocalId) {
+                AddLocalIdScene(viewModel: AddLocalIdViewModel(container: container))
+            }]
+            : [SettingsMenu(AccountSettings.showLocalId,
+                            content: currentUser?.localId ?? "(없음)"),
+               SettingsMenu(AccountSettings.changePassword)])
         menu.append(currentUser?.fbName == nil
-                    ? [SettingsMenu(AccountSettings.makeFbConnection)]
-                    : [SettingsMenu(AccountSettings.showFbName,
-                                    content: currentUser?.fbName ?? "(없음)"),
-                       SettingsMenu(AccountSettings.deleteFbConnection)])
+            ? [SettingsMenu(AccountSettings.makeFbConnection)]
+            : [SettingsMenu(AccountSettings.showFbName,
+                            content: currentUser?.fbName ?? "(없음)"),
+               SettingsMenu(AccountSettings.deleteFbConnection)])
         menu.append([SettingsMenu(AccountSettings.showEmail,
                                   content: currentUser?.email ?? "(없음)")])
         menu.append([SettingsMenu(AccountSettings.deleteAccount, destructive: true)])
         return menu
     }
-    
+
     func fetchUser() async {
         do {
             try await userService.fetchUser()
@@ -52,7 +52,7 @@ class AccountSettingViewModel: BaseViewModel, ObservableObject {
             print("user error")
         }
     }
-    
+
     private var currentUser: User? {
         appState.user.current
     }
