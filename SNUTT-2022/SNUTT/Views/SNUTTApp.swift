0afa417bd749a71e483ac9cc3fab7484083f53b2
shp7724 <shp7724@users.noreply.github.com>
2022-09-25 00:49:15 +0000

    Apply SwiftFormat changes

--- SNUTT-2022/SNUTT/Views/SNUTTApp.swift
+++ SNUTT-2022/SNUTT/Views/SNUTTApp.swift
@@ -13,7 +13,7 @@ struct SNUTTApp: App {
 
     init() {
         appEnvironment = AppEnvironment.bootstrap()
-        
+
         /// We need to load access token ASAP in order to determine which screen to show first.
         /// Note that this should run synchronously on the main thread.
         appEnvironment.container.services.authService.loadAccessTokenDuringBootstrap()
