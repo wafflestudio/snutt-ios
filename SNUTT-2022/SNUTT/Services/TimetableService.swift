c9a8e06a7bbb6b098bcb6e510f39af32ec23d783
Park Shin Hong <shp7724@naver.com>
2022-09-27 17:12:36 +0900

    fix: check user id before setting timetable

--- SNUTT-2022/SNUTT/Services/TimetableService.swift
+++ SNUTT-2022/SNUTT/Services/TimetableService.swift
@@ -43,7 +43,9 @@ struct TimetableService: TimetableServiceProtocol {
     func fetchRecentTimetable() async throws {
         if let localData = userDefaultsRepository.get(TimetableDto.self, key: .currentTimetable) {
             let localTimetable = Timetable(from: localData)
-            await updateState(to: localTimetable) // 일단 저장된 시간표로 상태 업데이트
+            if appState.user.current?.localId == localTimetable.userId {
+                await updateState(to: localTimetable) // 일단 저장된 시간표로 상태 업데이트
+            }
             try await fetchTimetable(timetableId: localTimetable.id) // API 요청을 통해 시간표 최신화
             return
         }
