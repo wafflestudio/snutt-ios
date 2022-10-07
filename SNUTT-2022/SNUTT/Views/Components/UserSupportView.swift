8431cdceb6642ffdec2ea3da7c64397132a2b02d
Park Shin Hong <shp7724@naver.com>
2022-10-07 21:17:48 +0900

    feat: move cursor on submit

--- SNUTT-2022/SNUTT/Views/Components/UserSupportView.swift
+++ SNUTT-2022/SNUTT/Views/Components/UserSupportView.swift
@@ -9,26 +9,49 @@ import SwiftUI
 
 struct UserSupportView: View {
     var sendFeedback: (String, String) async -> Bool // email, message -> success
-
+    
     @State private var email: String = ""
     @State private var content: String = ""
     @State private var alertSendFeedback: Bool = false
     @Environment(\.presentationMode) private var mode
-
+    
     var isButtonDisabled: Bool {
         email.isEmpty || content.isEmpty
     }
-
+    
+    enum FocusedField {
+        case email, content
+    }
+    
+    @FocusState private var focusedField: FocusedField?
+    
+    @State var showEmailValidationError: Bool = false
+    
     var body: some View {
         Form {
-            Section(header: Text("이메일 주소")) {
+            Section(header: Text("이메일 주소"), footer: Group {
+                if showEmailValidationError {
+                    Text("유효한 이메일 형식이 아닙니다.")
+                        .foregroundColor(.red)
+                }
+            }) {
                 TextField("이메일", text: $email)
+                    .focused($focusedField, equals: .email)
+                    .onSubmit {
+                        focusedField = .content
+                    }
             }
             Section(header: Text("문의 내용"), footer: Text("불편한 점이나 버그를 제보해주세요.")) {
                 TextEditor(text: $content)
-                    .frame(minHeight: 300)
+                    .focused($focusedField, equals: .content)
+                    .frame(minHeight: 200)
             }
         }
+        .onChange(of: focusedField, perform: { newValue in
+            if newValue == .content {
+                showEmailValidationError = !Validation.check(email: email)
+            }
+        })
         .navigationBarTitleDisplayMode(.inline)
         .navigationTitle("개발자 괴롭히기")
         .toolbar {
@@ -45,6 +68,7 @@ struct UserSupportView: View {
             Button("취소", role: .cancel, action: {})
             Button("전송", role: .none, action: {
                 Task {
+                    showEmailValidationError = !Validation.check(email: email)
                     let success = await sendFeedback(email, content)
                     if success {
                         mode.wrappedValue.dismiss()
