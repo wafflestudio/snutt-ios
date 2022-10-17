f6caad0eda07bc37be86753e93a6d78f2f9d1afc
shp7724 <shp7724@users.noreply.github.com>
2022-10-17 06:11:00 +0000

    Apply SwiftFormat changes

--- SNUTT-2022/SNUTT/ViewModels/SettingViewModel.swift
+++ SNUTT-2022/SNUTT/ViewModels/SettingViewModel.swift
@@ -38,7 +38,7 @@ class SettingViewModel: BaseViewModel {
         }
         return "v\(appVersion)-\(appType).\(buildNumber)"
     }
-    
+
     func sendFeedback(email: String, message: String) async -> Bool {
         if !Validation.check(email: email) {
             services.globalUIService.presentErrorAlert(error: .INVALID_EMAIL)
