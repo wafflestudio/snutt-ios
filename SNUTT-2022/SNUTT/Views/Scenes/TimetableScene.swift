5222fd2e9a5d7b45a27c98bd2141902202390a73
peng-u-0807-macbook <penguin0807@snu.ac.kr>
2022-11-05 15:26:53 +0900

    feat: share screenshot of current timetable

--- SNUTT-2022/SNUTT/Views/Scenes/TimetableScene.swift
+++ SNUTT-2022/SNUTT/Views/Scenes/TimetableScene.swift
@@ -10,81 +10,103 @@ import SwiftUI
 struct TimetableScene: View {
     @State private var pushToListScene = false
     @State private var pushToNotiScene = false
+    @State private var isShareSheetOpened = false
+    @State private var screenshot: UIImage = .init()
     @ObservedObject var viewModel: TimetableViewModel
 
     var body: some View {
-        TimetableZStack(current: viewModel.currentTimetable, config: viewModel.configuration)
-            .animation(.customSpring, value: viewModel.timetableState.current?.id)
-            // navigate programmatically, because NavigationLink inside toolbar doesn't work
-            .background(
-                Group {
-                    NavigationLink(destination: LectureListScene(viewModel: .init(container: viewModel.container)), isActive: $pushToListScene) { EmptyView() }
-                    NavigationLink(destination: NotificationList(notifications: viewModel.notifications,
-                                                                 initialFetch: viewModel.fetchInitialNotifications,
-                                                                 fetchMore: viewModel.fetchMoreNotifications), isActive: $pushToNotiScene) { EmptyView() }
+        GeometryReader { reader in
+            TimetableZStack(current: viewModel.currentTimetable, config: viewModel.configuration)
+                .animation(.customSpring, value: viewModel.timetableState.current?.id)
+                // navigate programmatically, because NavigationLink inside toolbar doesn't work
+                .background(
+                    Group {
+                        NavigationLink(destination: LectureListScene(viewModel: .init(container: viewModel.container)), isActive: $pushToListScene) { EmptyView() }
+                        NavigationLink(destination: NotificationList(notifications: viewModel.notifications,
+                                                                     initialFetch: viewModel.fetchInitialNotifications,
+                                                                     fetchMore: viewModel.fetchMoreNotifications), isActive: $pushToNotiScene) { EmptyView() }
+                    }
+                )
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        HStack {
+                            NavBarButton(imageName: "nav.menu") {
+                                viewModel.setIsMenuOpen(true)
+                            }
+                            .circleBadge(condition: viewModel.isNewCourseBookAvailable)
+
+                            Text(viewModel.timetableTitle)
+                                .font(STFont.title)
+                                .minimumScaleFactor(0.9)
+                                .lineLimit(1)
+                            Text("(\(viewModel.totalCredit) 학점)")
+                                .font(STFont.details)
+                                .foregroundColor(Color(UIColor.secondaryLabel))
+
+                            Spacer()
+
+                            NavBarButton(imageName: "nav.list") {
+                                pushToListScene = true
+                            }
+
+                            NavBarButton(imageName: "nav.share") {
+                                self.screenshot = body.takeScreenshot(size: reader.size)
+                                isShareSheetOpened = true
+                            }
+
+                            NavBarButton(imageName: "nav.alarm.off") {
+                                pushToNotiScene = true
+                            }
+                            .circleBadge(condition: viewModel.unreadCount > 0)
+                        }
+                    }
                 }
-            )
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    HStack {
-                        NavBarButton(imageName: "nav.menu") {
-                            viewModel.setIsMenuOpen(true)
-                        }
-                        .circleBadge(condition: viewModel.isNewCourseBookAvailable)
-
-                        Text(viewModel.timetableTitle)
-                            .font(STFont.title)
-                            .minimumScaleFactor(0.9)
-                            .lineLimit(1)
-                        Text("(\(viewModel.totalCredit) 학점)")
-                            .font(STFont.details)
-                            .foregroundColor(Color(UIColor.secondaryLabel))
-
-                        Spacer()
-
-                        NavBarButton(imageName: "nav.list") {
-                            pushToListScene = true
-                        }
-
-                        NavBarButton(imageName: "nav.share") {
-                            print("share tapped")
-                        }
-
-                        NavBarButton(imageName: "nav.alarm.off") {
-                            pushToNotiScene = true
-                        }
-                        .circleBadge(condition: viewModel.unreadCount > 0)
-                    }
+                .sheet(isPresented: $isShareSheetOpened) { [screenshot] in
+                    ActivityViewController(activityItems: [screenshot])
                 }
-            }
-            .onLoad {
-                // make the following three api calls execute concurrently
-                await withTaskGroup(of: Void.self, body: { group in
-                    group.addTask {
-                        await viewModel.fetchTimetableList()
-                    }
-                    group.addTask {
-                        await viewModel.fetchRecentTimetable()
-                    }
-                    group.addTask {
-                        await viewModel.fetchCourseBookList()
-                    }
-                    group.addTask {
-                        await viewModel.fetchInitialNotifications(updateLastRead: false)
-                    }
-                    group.addTask {
-                        await viewModel.fetchNotificationsCount()
-                    }
-                })
-            }
-//            .onAppear {
-//                Task {
-//                    await viewModel.fetchNotificationsCount()
-//                }
-//            }
+                .onLoad {
+                    // make the following three api calls execute concurrently
+                    await withTaskGroup(of: Void.self, body: { group in
+                        group.addTask {
+                            await viewModel.fetchTimetableList()
+                        }
+                        group.addTask {
+                            await viewModel.fetchRecentTimetable()
+                        }
+                        group.addTask {
+                            await viewModel.fetchCourseBookList()
+                        }
+                        group.addTask {
+                            await viewModel.fetchInitialNotifications(updateLastRead: false)
+                        }
+                        group.addTask {
+                            await viewModel.fetchNotificationsCount()
+                        }
+                    })
+                }
+    //            .onAppear {
+    //                Task {
+    //                    await viewModel.fetchNotificationsCount()
+    //                }
+    //            }
 
+        }
         let _ = debugChanges()
+    }
+}
+
+// MARK: ActivityViewController
+
+struct ActivityViewController: UIViewControllerRepresentable {
+    var activityItems: [Any]
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ActivityViewController>) -> UIActivityViewController {
+        let controller = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+        return controller
+    }
+
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: UIViewControllerRepresentableContext<ActivityViewController>) {
     }
 }
 
