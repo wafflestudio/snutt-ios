9cf743db72c1812787a5fcfef9f53fe0e19332b5
shinhong_park <shp7724@snu.ac.kr>
2022-11-12 15:44:55 +0900

    다국어 지원 파일 추가 (#147)
    feat: add localization file

--- SNUTT-2022/SNUTT/Models/ErrorCode.swift
+++ SNUTT-2022/SNUTT/Models/ErrorCode.swift
@@ -91,7 +91,7 @@ enum ErrorCode: Int {
     var errorTitle: String {
         switch self {
         case .SERVER_FAULT:
-            return "서버 오류"
+            return "SERVER_ERROR".localized
         case .NO_NETWORK:
             return "네트워크 오류"
         case .NO_FB_ID_OR_TOKEN,
