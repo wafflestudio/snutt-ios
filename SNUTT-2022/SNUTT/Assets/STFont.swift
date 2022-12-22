f06d8b2e40b96387c6e0b02a77a4cf3862e86780
peng-u-0807 <penguin0807@snu.ac.kr>
2022-12-22 17:31:06 +0900

    Fix typo + remove unused lines

--- SNUTT-2022/SNUTT/Assets/STFont.swift
+++ SNUTT-2022/SNUTT/Assets/STFont.swift
@@ -8,22 +8,24 @@
 import SwiftUI
 
 struct STFont {
-    /// 시간표 이름 등 강조할 때 사용하는 제목 글씨
+    /// 시간표 이름 등의 제목 (17, Bold)
     static let title: Font = .system(size: 17, weight: .bold)
 
-    /// 강의명 등 제목을 표기할 때 사용하는 굵은 글씨
+    /// 강의명 등의 제목 (14, Bold)
     static let subheading: Font = .system(size: 14, weight: .bold)
 
-    /// 강의 상세 화면에서 강의명 등 제목을 표기할 때 사용하는 얇은 글씨
+    /// 강의 상세 화면의 제목 (14, Regular)
     static let detailLabel: Font = .system(size: 14, weight: .regular)
 
-    /// 시간표 블록에서 강의명을 표기할 때 사용하는 글씨
+    /// 시간표 블록의 강의명 (11, Regular)
     static let lectureBlockTitle: Font = .system(size: 11, weight: .regular)
 
-    /// 시간표 블록에서 강의 장소를 표기할 때 사용하는 글씨
+    /// 시간표 블록의 강의 장소 (11, Bold)
     static let lectureBlockPlace: Font = .system(size: 11, weight: .bold)
 
-    /// 상세 정보를 표기할 때 사용하는 작은 글씨
+    /// 상세 정보 (12, Regular)
     static let details: Font = .system(size: 12, weight: .regular)
+    
+    /// 상세 정보 (12, Semibold)
     static let detailsSemibold: Font = .system(size: 12, weight: .semibold)
 }
--- SNUTT-2022/SNUTT/Utils/ValidationUtils.swift
+++ SNUTT-2022/SNUTT/Utils/ValidationUtils.swift
@@ -17,32 +17,10 @@ struct Validation {
         "9][0-9]?|[\\p{L}0-9-]*[\\p{L}0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21" +
         "-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
 
-    static func check(id: String) -> Bool {
-        guard let _ = id.range(of: "^[a-z0-9]{4,32}$", options: [.regularExpression, .caseInsensitive]) else {
-            return false
-        }
-        return true
-    }
-
-    static func check(password: String) -> Bool {
-        guard let _ = password.range(of: "^(?=.*\\d)(?=.*[a-z])\\S{6,20}$", options: [.regularExpression, .caseInsensitive]) else {
-            return false
-        }
-        return true
-    }
-
     static func check(email: String) -> Bool {
         guard let _ = email.range(of: emailRegex, options: [.regularExpression, .caseInsensitive]) else {
             return false
         }
         return true
-    }
-
-    static func check(id _: String, password _: String, check _: String) -> Bool {
-        return true
-    }
-
-    static func check(password: String, check: String) -> Bool {
-        return password == check
     }
 }
