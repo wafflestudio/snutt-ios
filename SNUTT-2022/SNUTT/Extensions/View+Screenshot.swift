6c109348b91fd0746cbad6abf9e1b767c669cec6
shinhong_park <shp7724@snu.ac.kr>
2023-03-07 00:11:11 +0900

    use preferredColorScheme when taking screenshot

--- SNUTT-2022/SNUTT/Extensions/View+Screenshot.swift
+++ SNUTT-2022/SNUTT/Extensions/View+Screenshot.swift
@@ -10,10 +10,11 @@ import SwiftUI
 extension View {
     /// Take a screenshot of View. The default size is `Full-sized screen` of device. Use `GeometryReader` to get an exact-sized screenshot of View.
     /// The origin of screenshot is `.zero`.
-    func takeScreenshot(size: CGSize = UIScreen.main.bounds.size) -> UIImage {
+    func takeScreenshot(size: CGSize = UIScreen.main.bounds.size, preferredColorScheme: ColorScheme? = nil) -> UIImage {
         let viewController = UIHostingController(rootView: self.ignoresSafeArea(.all))
         viewController.view.frame = CGRect(origin: .zero, size: size)
-        
+        viewController.overrideUserInterfaceStyle = UIUserInterfaceStyle(preferredColorScheme)
+
         let renderer = UIGraphicsImageRenderer(bounds: viewController.view.bounds)
         return renderer.image { context in
             viewController.view.drawHierarchy(in: viewController.view.bounds, afterScreenUpdates: true)
