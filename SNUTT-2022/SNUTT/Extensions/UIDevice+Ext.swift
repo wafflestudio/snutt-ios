58b1af80d50d631a8c1e527ce7b878ca41bc72ac
Yoorim Choi <penguin0807@snu.ac.kr>
2023-06-28 00:06:51 +0900

    Add device info & id to header (#235)
    * add device info header
    
    * add default value for modelName

---
+++ SNUTT-2022/SNUTT/Extensions/UIDevice+Ext.swift
@@ -0,0 +1,22 @@
+//
+//  UIDevice+Ext.swift
+//  SNUTT
+//
+//  Created by 최유림 on 2023/06/27.
+//
+
+import UIKit
+
+extension UIDevice {
+    /// Returns the name of device model (ex: iPhone10,2 or iPad3,2)
+    var modelName: String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let data = Data(bytes: &systemInfo.machine, count: Int(_SYS_NAMELEN))
+        if let identifier = String(bytes: data, encoding: .ascii) {
+            return identifier.trimmingCharacters(in: .controlCharacters)
+        } else {
+            return "Unknown"
+        }
+    }
+}
