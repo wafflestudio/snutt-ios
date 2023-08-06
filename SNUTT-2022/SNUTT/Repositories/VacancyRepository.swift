be1bfbff32cbd14d7e4c05d8f82910aaaa87e5b7
shinhong_park <shp7724@snu.ac.kr>
2023-08-07 00:53:57 +0900

    Add vacancy router & repository

---
+++ SNUTT-2022/SNUTT/Repositories/VacancyRepository.swift
@@ -0,0 +1,46 @@
+//
+//  VacancyRepository.swift
+//  SNUTT
+//
+//  Created by 박신홍 on 2023/07/22.
+//
+
+import Alamofire
+import Foundation
+
+protocol VacancyRepositoryProtocol {
+    func fetchLectures() async throws -> [LectureDto]
+    func addLecture(lectureId: String) async throws
+    func deleteLecture(lectureId: String) async throws
+}
+
+class VacancyRepository: VacancyRepositoryProtocol {
+
+    private let session: Session
+
+    init(session: Session) {
+        self.session = session
+    }
+
+    func fetchLectures() async throws -> [LectureDto] {
+        let dto = try await session
+            .request(VacancyRouter.getLectures)
+            .serializingDecodable(VacancyResponseDto.self)
+            .handlingError()
+        return dto.lectures
+    }
+
+    func addLecture(lectureId: String) async throws {
+        try await session
+            .request(VacancyRouter.addLecture(lectureId: lectureId))
+            .serializingDecodable(Empty.self, emptyResponseCodes: [200])
+            .handlingError()
+    }
+
+    func deleteLecture(lectureId: String) async throws {
+        try await session
+            .request(VacancyRouter.deleteLecture(lectureId: lectureId))
+            .serializingDecodable(Empty.self, emptyResponseCodes: [200])
+            .handlingError()
+    }
+}
