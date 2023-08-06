4ba1344f204337b75049242fedf73b3697733fa0
shinhong_park <shp7724@snu.ac.kr>
2023-08-07 00:56:58 +0900

    Add UI components for vacancy service

---
+++ SNUTT-2022/SNUTT/Views/Components/Vacancy/VacancyLectureList.swift
@@ -0,0 +1,102 @@
+//
+//  VacancyLectureList.swift
+//  SNUTT
+//
+//  Created by 박신홍 on 2023/07/22.
+//
+
+import SwiftUI
+
+struct VacancyLectureList: View {
+    @ObservedObject var viewModel: VacancyScene.ViewModel
+    @Binding var editMode: EditMode
+    var isGuidePopupPresented: Bool
+    @State private var selection = Set<String>()
+
+    var body: some View {
+        ZStack {
+            List(selection: $selection) {
+                ForEach(viewModel.lectures, id: \.id) { lecture in
+                    VacancyLectureCell(lecture: lecture)
+                        .listRowInsets(EdgeInsets())
+                        .listRowBackground(lecture.hasVacancy ? STColor.vacancyRedBackground : .clear)
+                }
+            }
+            .tint(STColor.cyan)
+            .toolbar {
+                editButton
+            }
+            .listStyle(.plain)
+            .refreshable {
+                await viewModel.fetchLectures()
+            }
+            .environment(\.editMode, self.$editMode)
+            .safeAreaInset(edge: .bottom) {
+                if editMode.isEditing {
+                    deleteButton
+                        .transition(.opacity.combined(with: .move(edge: .bottom)).animation(.customSpring))
+                }
+            }
+            .animation(.customSpring, value: viewModel.lectures.count)
+            .animation(.customSpring, value: editMode)
+        }
+    }
+
+    private var deleteButton: some View {
+        Button {
+            let lectures = viewModel.lectures
+            let selection = selection
+            viewModel.lectures.removeAll(where: { selection.contains($0.id) })
+            editMode = .inactive
+            Task {
+                let lecturesToDelete = lectures.filter { lecture in
+                    selection.contains(lecture.id)
+                }
+                await viewModel.deleteLectures(lectures: lecturesToDelete)
+            }
+        } label: {
+            Text("선택한 강의 삭제")
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .contentShape(Rectangle())
+                .foregroundColor(.white)
+        }
+        .background(selection.isEmpty ? STColor.gray : STColor.cyan)
+        .disabled(selection.isEmpty)
+        .foregroundColor(Color(uiColor: .systemBackground))
+
+    }
+
+    private var editButton: some View {
+        Button(action: {
+            withAnimation {
+                self.editMode.toggle()
+            }
+            self.selection = Set<String>()
+        }) {
+            Text(self.editMode.title)
+        }
+        .disabled(viewModel.lectures.isEmpty || isGuidePopupPresented)
+    }
+}
+
+extension EditMode {
+    var title: String {
+        self == .active ? "취소" : "편집"
+    }
+
+    mutating func toggle() {
+        self = self == .active ? .inactive : .active
+    }
+}
+
+#if DEBUG
+struct VacancyLectureList_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            VacancyLectureList(viewModel: .init(container: .preview), editMode: .constant(.inactive), isGuidePopupPresented: false)
+        }
+    }
+}
+#endif
---
+++ SNUTT-2022/SNUTT/Views/Components/Vacancy/VacancySugangSnuButton.swift
@@ -0,0 +1,35 @@
+//
+//  VacancySugangSnuButton.swift
+//  SNUTT
+//
+//  Created by 박신홍 on 2023/08/06.
+//
+
+import SwiftUI
+
+struct VacancySugangSnuButton: View {
+    let action: () -> Void
+
+    var body: some View {
+        Button(action: {
+            action()
+        }) {
+            Text("수강신청 사이트")
+                .font(.system(size: 15, weight: .bold))
+                .foregroundColor(.white)
+                .padding(.horizontal, 15)
+                .padding(.vertical, 10)
+                .background(STColor.cyan)
+        }
+        .background(STColor.systemBackground)
+        .buttonStyle(.plain)
+        .clipShape(Capsule())
+        .shadow(color: .black.opacity(0.1), radius: 5)
+    }
+}
+
+struct VacancySugangSnuButton_Previews: PreviewProvider {
+    static var previews: some View {
+        VacancySugangSnuButton(action: { })
+    }
+}
