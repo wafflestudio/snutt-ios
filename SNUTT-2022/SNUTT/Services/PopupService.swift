5b388abb5908477e7c0c010e9229da1a39258e43
shinhong_park <shp7724@snu.ac.kr>
2023-09-11 14:48:25 +0900

    Mark the method private

--- SNUTT-2022/SNUTT/Services/PopupService.swift
+++ SNUTT-2022/SNUTT/Services/PopupService.swift
@@ -41,7 +41,19 @@ struct PopupService: PopupServiceProtocol {
         userDefaultsRepository.set([PopupDto].self, key: .popupList, value: mergedPopupDtos)
     }
 
-    func mergePopups(local: [PopupDto], into remote: [PopupDto]) -> [PopupDto] {
+    func dismissPopup(popup: Popup, dontShowForWhile: Bool) {
+        var currentPopupList = appState.popup.currentList
+        guard let firstPopupIndex = currentPopupList.firstIndex(where: { $0.id == popup.id }) else { return }
+        currentPopupList[firstPopupIndex].dismissedAt = Date()
+        currentPopupList[firstPopupIndex].dontShowForWhile = dontShowForWhile
+        appState.popup.currentList = currentPopupList
+        let currentPopupListDto = currentPopupList.compactMap { PopupDto(from: $0) }
+        userDefaultsRepository.set([PopupDto].self, key: .popupList, value: currentPopupListDto)
+    }
+}
+
+extension PopupService {
+    private func mergePopups(local: [PopupDto], into remote: [PopupDto]) -> [PopupDto] {
         let localPopupByKey = Dictionary(grouping: local, by: { $0.key })
         return remote.map { popupDto in
             guard let localPopup = localPopupByKey[popupDto.key]?.first else {
@@ -52,16 +64,6 @@ struct PopupService: PopupServiceProtocol {
             }
             return localPopup
         }
-    }
-
-    func dismissPopup(popup: Popup, dontShowForWhile: Bool) {
-        var currentPopupList = appState.popup.currentList
-        guard let firstPopupIndex = currentPopupList.firstIndex(where: { $0.id == popup.id }) else { return }
-        currentPopupList[firstPopupIndex].dismissedAt = Date()
-        currentPopupList[firstPopupIndex].dontShowForWhile = dontShowForWhile
-        appState.popup.currentList = currentPopupList
-        let currentPopupListDto = currentPopupList.compactMap { PopupDto(from: $0) }
-        userDefaultsRepository.set([PopupDto].self, key: .popupList, value: currentPopupListDto)
     }
 }
 
