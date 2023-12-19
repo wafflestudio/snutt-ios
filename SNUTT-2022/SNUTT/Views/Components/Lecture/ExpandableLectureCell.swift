949159e53cf85d8ec9b2c14c89f130aa68fa2ea9
shinhong_park <shp7724@snu.ac.kr>
2023-12-20 00:27:51 +0900

    `SearchLectureCell` → `ExpandableLectureCell` 리팩토링

---
+++ SNUTT-2022/SNUTT/Views/Components/Lecture/ExpandableLectureCell.swift
@@ -0,0 +1,310 @@
+//
+//  ExpandableLectureCell.swift
+//  SNUTT
+//
+//  Created by 박신홍 on 2023/12/17.
+//
+
+import SwiftUI
+import Combine
+
+struct ExpandableLectureCell: View {
+    @ObservedObject var viewModel: ViewModel
+
+    let lecture: Lecture
+    let isSelected: Bool
+    let isBookmarked: Bool
+    let isInCurrentTimetable: Bool
+    let isVacancyNotificationEnabled: Bool
+
+    @State private var isDetailPagePresented = false
+    @State private var isReviewWebViewPresented = false
+    @State private var isRemoveBookmarkAlertPresented = false
+    @State private var reviewDetailId: String? = nil
+
+    var body: some View {
+        ZStack {
+            if isSelected {
+                STColor.searchListForeground
+            }
+
+            VStack(spacing: 8) {
+                LectureHeaderRow(lecture: lecture)
+
+                if lecture.isCustom {
+                    LectureDetailRow(imageName: "tag.white", text: "")
+                } else {
+                    LectureDetailRow(imageName: "tag.white", text: "\(lecture.department), \(lecture.academicYear)")
+                }
+                LectureDetailRow(imageName: "clock.white", text: lecture.preciseTimeString)
+
+                LectureDetailRow(imageName: "map.white", text: lecture.placesString)
+
+                LectureDetailRow(imageName: "ellipsis.white", text: lecture.remark)
+
+                if isSelected {
+                    Spacer().frame(height: 5)
+
+                    HStack {
+                        LectureCellActionButton(
+                            icon: .asset(name: "search.detail"),
+                            text: "자세히"
+                        ) {
+                            isDetailPagePresented = true
+                        }
+
+                        LectureCellActionButton(
+                            icon: .asset(name: "search.evaluation"),
+                            text: "강의평"
+                        ) {
+                            reviewDetailId = await viewModel.fetchReviewDetailId(lecture: lecture)
+                            if let reviewId = reviewDetailId {
+                                viewModel.reloadReviewWebView(reviewId: reviewId)
+                                isReviewWebViewPresented = true
+                            }
+                        }
+                        
+                        LectureCellActionButton(
+                            icon: .asset(name: isBookmarked ? "search.bookmark.fill" : "search.bookmark"),
+                            text: "관심강좌"
+                        ) {
+                            if isBookmarked {
+                                isRemoveBookmarkAlertPresented = true
+                            } else {
+                                await viewModel.addBookmarkLecture(lecture)
+                            }
+                        }
+                        .alert("강의를 관심강좌에서 제외하시겠습니까?", isPresented: $isRemoveBookmarkAlertPresented) {
+                            Button("취소", role: .cancel, action: {})
+                            Button("확인", role: .destructive) {
+                                Task {
+                                    await viewModel.removeBookmarkLecture(lecture)
+                                }
+                            }
+                        }
+                        
+                        LectureCellActionButton(
+                            icon: .asset(name: isVacancyNotificationEnabled ? "search.vacancy.fill" : "search.vacancy"),
+                            text: "빈자리알림"
+                        ) {
+                            if isVacancyNotificationEnabled {
+                                await viewModel.removeVacancyLecture(lecture)
+                            } else {
+                                await viewModel.addVacancyLecture(lecture)
+                            }
+                        }
+                        
+                        LectureCellActionButton(
+                            icon: .asset(name: isInCurrentTimetable ? "search.remove.fill" : "search.add"),
+                            text: isInCurrentTimetable ? "제거하기" : "추가하기"
+                        ) {
+                            if isInCurrentTimetable {
+                                await viewModel.removeLecture(lecture)
+                            } else {
+                                await viewModel.addLecture(lecture)
+                            }
+                        }
+                    }
+                    /// This `sheet` modifier should be called on `HStack` to prevent animation glitch when `dismiss`ed.
+                    .sheet(isPresented: $isReviewWebViewPresented) {
+                        ReviewScene(
+                            viewModel: .init(container: viewModel.container),
+                            isMainWebView: false,
+                            detailId: reviewDetailId
+                        )
+                                .id(reviewDetailId)
+                    }
+                    .sheet(isPresented: $isDetailPagePresented) {
+                            NavigationView {
+                                LectureDetailScene(
+                                    viewModel: .init(container: viewModel.container),
+                                    lecture: lecture,
+                                    displayMode: .preview)
+                            }
+                    }
+                }
+            }
+            .foregroundColor(.white)
+            .padding(.vertical, 10)
+            .padding(.horizontal, 15)
+        }
+        .alert(viewModel.errorTitle, isPresented: $viewModel.isEmailVerifyAlertPresented, actions: {
+            Button("확인") {
+                viewModel.selectedTab = .review
+            }
+            Button("취소", role: .cancel) {}
+        }, message: {
+            Text(viewModel.errorMessage)
+        })
+        .alert(viewModel.errorTitle, isPresented: $viewModel.isLectureOverlapped) {
+            Button {
+                Task {
+                    await viewModel.forciblyAddLecture(lecture)
+                }
+            } label: {
+                Text("확인")
+            }
+            Button("취소", role: .cancel) {
+                viewModel.isLectureOverlapped = false
+            }
+        } message: {
+            Text(viewModel.errorMessage)
+        }
+        .alert("관심강좌", isPresented: $viewModel.isFirstBookmarkAlertPresented) {
+            Button("확인", role: .cancel, action: {
+                viewModel.isFirstBookmarkAlertPresented = false
+            })
+        } message: {
+            Text("시간표 우측 상단에서 선택한 관심강좌 목록을 확인해보세요")
+        }
+    }
+}
+
+
+
+extension ExpandableLectureCell {
+    class ViewModel: BaseViewModel, ObservableObject {
+        @Published var isLectureOverlapped: Bool = false
+        @Published var isFirstBookmarkAlertPresented: Bool = false
+        @Published var isEmailVerifyAlertPresented = false
+        var errorTitle: String = ""
+        var errorMessage: String = ""
+
+        private var searchState: SearchState {
+            appState.search
+        }
+
+        private var timetableState: TimetableState {
+            appState.timetable
+        }
+
+        var selectedTab: TabType {
+            get { appState.system.selectedTab }
+            set { services.globalUIService.setSelectedTab(newValue) }
+        }
+    }
+}
+
+extension ExpandableLectureCell.ViewModel {
+    func addLecture(_ lecture: Lecture) async {
+        do {
+            try await services.lectureService.addLecture(lecture: lecture)
+        } catch {
+            if let error = error.asSTError {
+                if error.code == .LECTURE_TIME_OVERLAP {
+                    isLectureOverlapped = true
+                    errorTitle = error.title
+                    errorMessage = error.content
+                } else {
+                    services.globalUIService.presentErrorAlert(error: error)
+                }
+            }
+        }
+    }
+
+    func forciblyAddLecture(_ lecture: Lecture) async {
+        do {
+            try await services.lectureService.addLecture(lecture: lecture, isForced: true)
+        } catch {
+            services.globalUIService.presentErrorAlert(error: error)
+        }
+    }
+
+    func removeLecture(_ lecture: Lecture) async {
+        guard isInCurrentTimetable(lecture: lecture)
+        else { return }
+        do {
+            try await services.lectureService.deleteLecture(lecture: lecture)
+            services.searchService.setSelectedLecture(nil)
+        } catch {
+            services.globalUIService.presentErrorAlert(error: error)
+        }
+    }
+
+    func addVacancyLecture(_ lecture: Lecture) async {
+        do {
+            try await services.vacancyService.addLecture(lecture: lecture)
+        } catch {
+            services.globalUIService.presentErrorAlert(error: error)
+        }
+    }
+
+    func removeVacancyLecture(_ lecture: Lecture) async {
+        do {
+            try await services.vacancyService.deleteLectures(lectures: [lecture])
+        } catch {
+            services.globalUIService.presentErrorAlert(error: error)
+        }
+    }
+
+    func addBookmarkLecture(_ lecture: Lecture) async {
+        isFirstBookmarkAlertPresented = appState.timetable.isFirstBookmark ?? false
+        do {
+            try await services.lectureService.bookmarkLecture(lecture: lecture)
+        } catch {
+            services.globalUIService.presentErrorAlert(error: error)
+        }
+    }
+
+    func removeBookmarkLecture(_ lecture: Lecture) async {
+        guard isBookmarked(lecture: lecture) else { return }
+        do {
+            try await services.lectureService.undoBookmarkLecture(lecture: lecture)
+        } catch {
+            services.globalUIService.presentErrorAlert(error: error)
+        }
+    }
+
+    func fetchReviewDetailId(lecture: Lecture) async -> String? {
+        do {
+            return try await services.lectureService.fetchReviewId(courseNumber: lecture.courseNumber, instructor: lecture.instructor)
+        } catch let error as STError where error.code == .EMAIL_NOT_VERIFIED {
+            errorTitle = error.title
+            errorMessage = error.content
+            isEmailVerifyAlertPresented = true
+        } catch {
+            services.globalUIService.presentErrorAlert(error: error)
+        }
+        return nil
+    }
+
+    func reloadReviewWebView(reviewId: String) {
+        services.globalUIService.sendDetailWebViewReloadSignal(
+            url: WebViewType.reviewDetail(id: reviewId).url
+        )
+    }
+
+    private func isBookmarked(lecture: Lecture) -> Bool {
+        timetableState.bookmark?.lectures
+            .contains(where: { $0.isEquivalent(with: lecture) }) ?? false
+    }
+
+    private func isInCurrentTimetable(lecture: Lecture) -> Bool {
+        timetableState.current?.lectures
+            .contains { $0.isEquivalent(with: lecture) } ?? false
+    }
+
+    private func isVacancyNotificationEnabled(lecture: Lecture) -> Bool {
+        appState.vacancy.lectures
+            .contains { $0.isEquivalent(with: lecture) }
+    }
+}
+
+extension ObservableObject where Self.ObjectWillChangePublisher == ObservableObjectPublisher {
+    func objectWillChangeWhen(triggeredBy publishers: Any...) -> AnyCancellable {
+        let typeErasedPublishers = publishers.compactMap {
+            let publisher = $0 as? (any Publisher)
+            return publisher?.eraseToAnyVoidPublisher()
+        }
+        return Publishers.MergeMany(typeErasedPublishers)
+            .sink { [weak self] _ in self?.objectWillChange.send() }
+    }
+}
+
+extension Publisher {
+    func eraseToAnyVoidPublisher() -> AnyPublisher<Void, Never> {
+        return map { _ in () }
+            .replaceError(with: ())
+            .eraseToAnyPublisher()
+    }
+}
---
+++ SNUTT-2022/SNUTT/Views/Components/Lecture/ExpandableLectureList.swift
@@ -0,0 +1,87 @@
+//
+//  ExpandableLectureList.swift
+//  SNUTT
+//
+//  Created by 박신홍 on 2023/12/17.
+//
+
+import SwiftUI
+import Combine
+
+struct ExpandableLectureList: View {
+    @ObservedObject var viewModel: ViewModel
+
+    let lectures: [Lecture]
+    @Binding var selectedLecture: Lecture?
+    var fetchMoreLectures: (() async -> Void)? = nil
+
+    var body: some View {
+        let _ = debugChanges()
+        ScrollView {
+            LazyVStack(spacing: 0) {
+                ForEach(lectures) { lecture in
+                    ExpandableLectureCell(
+                        viewModel: .init(container: viewModel.container),
+                        lecture: lecture,
+                        isSelected: lecture.id == selectedLecture?.id,
+                        isBookmarked: viewModel.isBookmarked(lecture: lecture),
+                        isInCurrentTimetable: viewModel.isInCurrentTimetable(lecture: lecture),
+                        isVacancyNotificationEnabled: viewModel.isVacancyNotificationEnabled(lecture: lecture))
+
+                    .task {
+                        if lecture.id == lectures.last?.id {
+                            await fetchMoreLectures?()
+                        }
+                    }
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        if selectedLecture?.id != lecture.id {
+                            selectedLecture = lecture
+                        }
+                    }
+                }
+            }
+        }
+        .scrollDismissesKeyboardInteractively()
+
+    }
+}
+
+
+
+extension ExpandableLectureList {
+    class ViewModel: BaseViewModel, ObservableObject {
+        private var timetableState: TimetableState {
+            appState.timetable
+        }
+
+        private var cancellables: Set<AnyCancellable> = .init()
+
+        override init(container: DIContainer) {
+                    super.init(container: container)
+
+                    objectWillChangeWhen(triggeredBy:
+                        timetableState.$bookmark,
+                        timetableState.$current,
+                        appState.vacancy.$lectures
+                    )
+                    .store(in: &cancellables)
+                }
+
+            func isBookmarked(lecture: Lecture) -> Bool {
+                timetableState.bookmark?.lectures
+                    .contains(where: { $0.isEquivalent(with: lecture) }) ?? false
+            }
+        
+            func isInCurrentTimetable(lecture: Lecture) -> Bool {
+                timetableState.current?.lectures
+                    .contains { $0.isEquivalent(with: lecture) } ?? false
+            }
+
+            func isVacancyNotificationEnabled(lecture: Lecture) -> Bool {
+                appState.vacancy.lectures
+                    .contains { $0.isEquivalent(with: lecture) }
+            }
+    }
+}
+
