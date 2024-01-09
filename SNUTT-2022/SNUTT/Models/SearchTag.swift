37953b7b1b4e71d623318c1db64c028dcd798dd2
peng-u-0807 <penguin0807@snu.ac.kr>
2024-01-09 18:48:27 +0900

    Add Timerange search tag

--- SNUTT-2022/SNUTT/Models/SearchTag.swift
+++ SNUTT-2022/SNUTT/Models/SearchTag.swift
@@ -28,9 +28,12 @@ extension SearchTagList {
                   let tagType = SearchTagType(rawValue: key) else { continue }
             searchTags.append(contentsOf: tagStrings.map { SearchTag(type: tagType, text: $0) })
         }
+        
+        // 시간
+        searchTags.append(.init(type: .time, text: TimeType.empty.rawValue))
+        searchTags.append(.init(type: .time, text: TimeType.range.rawValue))
 
-        // 기타 태그는 직접 추가해주어야 한다.
-        searchTags.append(.init(type: .etc, text: EtcType.empty.rawValue))
+        // 기타
         searchTags.append(.init(type: .etc, text: EtcType.english.rawValue))
         searchTags.append(.init(type: .etc, text: EtcType.army.rawValue))
 
@@ -44,6 +47,7 @@ enum SearchTagType: String, CaseIterable {
     case department
     case academicYear = "academic_year"
     case credit
+    case time
     case category
     case etc
 
@@ -53,6 +57,7 @@ enum SearchTagType: String, CaseIterable {
         case .classification: return Color(hex: "#e5731c")
         case .credit: return Color(hex: "#8bbb1a")
         case .department: return Color(hex: "#0cada6")
+        case .time: return Color(hex: "#5BA0D7")
         case .category: return Color(hex: "#9c45a0")
         case .etc: return Color(hex: "#AF56B3")
         }
@@ -64,6 +69,7 @@ enum SearchTagType: String, CaseIterable {
         case .classification: return Color(hex: "#f58d3d")
         case .credit: return Color(hex: "#a6d930")
         case .department: return Color(hex: "#1bd0c9")
+        case .time: return Color(hex: "#1D99E8")
         case .category: return Color(hex: "#af56b3")
         case .etc: return Color(hex: "#AF56B3")
         }
@@ -75,21 +81,24 @@ enum SearchTagType: String, CaseIterable {
         case .classification: return "분류"
         case .credit: return "학점"
         case .department: return "학과"
+        case .time: return "시간"
         case .category: return "교양분류"
         case .etc: return "기타"
         }
     }
 }
 
-enum EtcType: String {
+enum TimeType: String {
     case empty = "빈 시간대"
+    case range = "시간대 직접 선택"
+}
+
+enum EtcType: String {
     case english = "영어진행 강의"
     case army = "군휴학 원격수업"
 
     var code: String? {
         switch self {
-        case .empty:
-            return nil // should not be included in parameters; use timeMasks instead
         case .english:
             return "E"
         case .army:
