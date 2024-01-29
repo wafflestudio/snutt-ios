67542e5d8e6d970c73d1fcda6f9c7b291668d25a
peng-u-0807 <penguin0807@snu.ac.kr>
2024-01-29 15:48:14 +0900

    Change `startMInute`, `endMinute` to computed property

--- SNUTT-2022/SNUTT/ViewModels/LectureListViewModel.swift
+++ SNUTT-2022/SNUTT/ViewModels/LectureListViewModel.swift
@@ -19,7 +19,7 @@ class LectureListViewModel: BaseViewModel, ObservableObject {
     var placeholderLecture: Lecture {
         var lecture: Lecture = .init(from: .init(_id: UUID().uuidString, lecture_id: nil, classification: nil, department: nil, academic_year: nil, course_title: "새로운 강의", credit: 0, class_time: nil, class_time_json: [], class_time_mask: [], instructor: "", remark: nil, category: nil, course_number: nil, lecture_number: nil, created_at: nil, updated_at: nil, color: nil, colorIndex: 1, wasFull: false, quota: nil, registrationCount: nil, freshmanQuota: nil))
         lecture.theme = appState.timetable.current?.theme ?? .snutt
-        lecture.timePlaces.append(.init(id: UUID().uuidString, day: .mon, startTime: .init(hour: 9, minute: 0), endTime: .init(hour: 10, minute: 0), startMinute: 540, endMinute: 600, place: "", isCustom: true, isTemporary: true))
+        lecture.timePlaces.append(.init(id: UUID().uuidString, day: .mon, startTime: .init(hour: 9, minute: 0), endTime: .init(hour: 10, minute: 0), place: "", isCustom: true, isTemporary: true))
         return lecture
     }
 }
