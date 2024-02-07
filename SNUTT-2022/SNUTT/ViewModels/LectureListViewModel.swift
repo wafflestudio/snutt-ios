a834e019f9b458423ef40b74d16b424ab16956f5
chaemin2001 <chaemin2001@snu.ac.kr>
2024-02-07 20:47:08 +0900

    Revert

--- SNUTT-2022/SNUTT/ViewModels/LectureListViewModel.swift
+++ SNUTT-2022/SNUTT/ViewModels/LectureListViewModel.swift
@@ -17,7 +17,7 @@ class LectureListViewModel: BaseViewModel, ObservableObject {
     }
 
     var placeholderLecture: Lecture {
-        var lecture: Lecture = .init(from: .init(_id: UUID().uuidString, lecture_id: nil, classification: nil, department: nil, academic_year: nil, course_title: "새로운 강의", credit: 0, class_time: nil, class_time_json: [], class_time_mask: [], instructor: "", remark: nil, category: nil, course_number: nil, lecture_number: nil, created_at: nil, updated_at: nil, color: nil, colorIndex: 0, wasFull: false, quota: nil, registrationCount: nil, freshmanQuota: nil))
+        var lecture: Lecture = .init(from: .init(_id: UUID().uuidString, lecture_id: nil, classification: nil, department: nil, academic_year: nil, course_title: "새로운 강의", credit: 0, class_time: nil, class_time_json: [], class_time_mask: [], instructor: "", remark: nil, category: nil, course_number: nil, lecture_number: nil, created_at: nil, updated_at: nil, color: nil, colorIndex: 1, wasFull: false, quota: nil, registrationCount: nil, freshmanQuota: nil))
         lecture.timePlaces.append(.init(id: UUID().uuidString,
                                         day: .mon,
                                         startTime: .init(hour: 9, minute: 0),
