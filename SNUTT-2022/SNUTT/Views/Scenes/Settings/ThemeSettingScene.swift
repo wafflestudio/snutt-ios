53e0056c8b81524b3e7b5ad2d69399a1df1d17f8
chaemin2001 <chaemin2001@snu.ac.kr>
2024-03-20 05:43:00 +0900

    Match with Figma

--- SNUTT-2022/SNUTT/Views/Scenes/Settings/ThemeSettingScene.swift
+++ SNUTT-2022/SNUTT/Views/Scenes/Settings/ThemeSettingScene.swift
@@ -61,22 +61,22 @@ struct ThemeSettingScene: View {
 }
 
 @ViewBuilder private func infoView() -> some View {
-    VStack(alignment: .leading, spacing: 6) {
-        HStack {
+    VStack(alignment: .leading, spacing: 4) {
+        HStack(spacing: 6) {
             Image("vacancy.info")
                 .resizable()
                 .frame(width: 14, height: 14)
             Text("테마는 어떻게 적용하나요?")
-                .font(STFont.detailsSemibold)
-                .foregroundColor(.secondary)
+                .font(.system(size: 12, weight: .bold))
+                .foregroundColor(STColor.gray2)
         }
         Text("시간표 적용은 시간표 목록 > 더보기 버튼 > 테마 설정에서 개별적으로 적용할 수 있어요.")
             .font(STFont.details)
-            .foregroundColor(.secondary)
+            .foregroundColor(STColor.gray2)
             .lineSpacing(1.3)
         Text("새로운 시간표에는 가장 최근 편집한 커스텀 테마가 적용돼요.")
             .font(STFont.details)
-            .foregroundColor(.secondary)
+            .foregroundColor(STColor.gray2)
     }
     .padding(.top, 25)
     .padding(.horizontal, -12)
