19fd230d92ebd40d113b62df7b8606d839c6e8aa
peng-u-0807 <penguin0807@snu.ac.kr>
2024-08-23 01:14:53 +0900

    Remove beta mark for map

--- SNUTT-2022/SNUTT/Views/Components/LectureMapView.swift
+++ SNUTT-2022/SNUTT/Views/Components/LectureMapView.swift
@@ -16,18 +16,14 @@ struct LectureMapView: View {
     @Environment(\.colorScheme) var colorScheme
 
     var body: some View {
-        ZStack(alignment: .topTrailing) {
-            KakaoMapView(showMapView: $showMapView,
-                         isMapNotInstalledAlertPresented: $isMapNotInstalledAlertPresented,
-                         colorScheme: colorScheme,
-                         buildings: buildings)
-                .onDisappear {
-                    showMapView = false
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-
-            Image("map.beta")
-        }
-        .alert("실행 가능한 지도 어플리케이션이 없습니다.", isPresented: $isMapNotInstalledAlertPresented, actions: {})
+        KakaoMapView(showMapView: $showMapView,
+                     isMapNotInstalledAlertPresented: $isMapNotInstalledAlertPresented,
+                     colorScheme: colorScheme,
+                     buildings: buildings)
+            .onDisappear {
+                showMapView = false
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .alert("실행 가능한 지도 어플리케이션이 없습니다.", isPresented: $isMapNotInstalledAlertPresented, actions: {})
     }
 }
