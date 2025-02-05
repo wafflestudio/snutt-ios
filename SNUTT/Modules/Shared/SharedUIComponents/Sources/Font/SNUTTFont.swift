88ba071ac7b4c1af6c12c77a5442941bc24d085a
peng-u-0807 <penguin0807@snu.ac.kr>
2025-02-05 20:19:07 +0900

    Add images for map + fonts

---
+++ SNUTT/Modules/Shared/SharedUIComponents/Sources/Font/SNUTTFont.swift
@@ -0,0 +1,48 @@
+//
+//  SNUTTFont.swift
+//  SNUTT
+//
+//  Copyright © 2025 wafflestudio.com. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+struct SNUTTFont {
+    static let bold17: UIFont = .systemFont(ofSize: 17, weight: .bold)
+
+    static let semibold16: UIFont = .systemFont(ofSize: 16, weight: .semibold)
+    static let regular16: UIFont = .systemFont(ofSize: 16)
+
+    static let semibold15: UIFont = .systemFont(ofSize: 15, weight: .semibold)
+    static let regular15: UIFont = .systemFont(ofSize: 15)
+
+    static let bold14: UIFont = .systemFont(ofSize: 14, weight: .bold)
+    static let semibold14: UIFont = .systemFont(ofSize: 14, weight: .semibold)
+    static let medium14: UIFont = .systemFont(ofSize: 14, weight: .medium)
+    static let regular14: UIFont = .systemFont(ofSize: 14)
+
+    static let regular13: UIFont = .systemFont(ofSize: 13)
+
+    static let regular12: UIFont = .systemFont(ofSize: 12)
+
+    static let bold11: UIFont = .systemFont(ofSize: 11, weight: .bold)
+    static let regular11: UIFont = .systemFont(ofSize: 11)
+}
+
+extension UIFont {
+    var font: Font {
+        Font(self as CTFont)
+    }
+}
+
+extension Text {
+    /// `percentage` : 100 if 100%
+    func lineHeight(with font: UIFont, percentage: CGFloat) -> some View {
+        let roundedSpacing = ((font.pointSize * (percentage - 100) / 100) * 0.5)
+        return self
+            .font(font.font)
+            .lineSpacing(roundedSpacing.rounded(.up) + 1)
+            .padding(.vertical, roundedSpacing.rounded(.down))
+    }
+}
