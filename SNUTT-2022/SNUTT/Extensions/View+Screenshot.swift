a522df27dfb977f86c98d642f91a76793f0f8ea4
Yoorim Choi <penguin0807@snu.ac.kr>
2025-07-15 15:30:27 +0900

    Remove `takeScreenshot()`

--- SNUTT-2022/SNUTT/Extensions/View+Screenshot.swift
+++ SNUTT-2022/SNUTT/Extensions/View+Screenshot.swift
@@ -8,22 +8,7 @@
 import SwiftUI
 
 extension View {
-    /// Take a screenshot of View. The default size is `Full-sized screen` of device. Use `GeometryReader` to get an exact-sized screenshot of View.
-    /// The origin of screenshot is `.zero`.
-    func takeScreenshot(
-        size: CGSize = UIScreen.main.bounds.size,
-        preferredColorScheme: ColorScheme? = nil
-    ) -> UIImage {
-        let viewController = UIHostingController(rootView: self.ignoresSafeArea())
-        viewController.view.frame = CGRect(origin: .zero, size: size)
-        viewController.overrideUserInterfaceStyle = UIUserInterfaceStyle(preferredColorScheme)
-
-        let renderer = UIGraphicsImageRenderer(bounds: viewController.view.bounds)
-        return renderer.image { context in
-            viewController.view.drawHierarchy(in: viewController.view.bounds, afterScreenUpdates: true)
-        }
-    }
-    
+    /// Create a png image of the timetable. The default size is `Full-sized screen` of device.
     func createTimetableImage(
         size: CGSize = UIScreen.main.bounds.size,
         timetable: Timetable,
