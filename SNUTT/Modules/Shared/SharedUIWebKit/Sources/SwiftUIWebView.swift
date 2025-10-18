36e2f8a3cf450521c963ab007b141fee39e597ea
shinhong_park <shp7724@snu.ac.kr>
2025-10-18 13:53:30 +0900

    ♻️ 웹뷰 성능 최적화: 웹뷰 재활용 풀 및 스크립트 핸들링 추가
    웹뷰 재활용을 통한 메모리 효율성 개선 및 자바스크립트 통신 기능 추가
    - WebViewRecycler: 웹뷰 객체 풀 관리로 메모리 재사용
    - SwiftUIWebView: ScriptMessageHandler를 통한 네이티브-웹 통신 지원
    - 웹뷰 설정 최적화 (지연 로딩, 쿠키 관리 개선)
    - dismantleUIView에서 웹뷰 재활용 풀로 반환

--- SNUTT/Modules/Shared/SharedUIWebKit/Sources/SwiftUIWebView.swift
+++ SNUTT/Modules/Shared/SharedUIWebKit/Sources/SwiftUIWebView.swift
@@ -10,32 +10,33 @@ import WebKit
 
 public struct SwiftUIWebView: UIViewRepresentable {
     let url: URL
-    let configuration: WKWebViewConfiguration
+    let cookies: [HTTPCookie]
     let allowsBackForwardNavigationGestures: Bool
     var onUpdate: ((WKWebView) -> Void)?
+    let scriptHandler: ScriptMessageHandler?
 
     public init(
         url: URL,
         cookies: [HTTPCookie] = [],
         allowsBackForwardNavigationGestures: Bool = true,
-        onUpdate: ((WKWebView) -> Void)? = nil
+        scriptHandler: ScriptMessageHandler? = nil,
+        onUpdate: ((WKWebView) -> Void)? = nil,
     ) {
         self.url = url
-        self.configuration = {
-            let configuration = WKWebViewConfiguration()
-            cookies.forEach {
-                configuration.websiteDataStore.httpCookieStore.setCookie($0)
-            }
-            return configuration
-        }()
+        self.cookies = cookies
         self.allowsBackForwardNavigationGestures = allowsBackForwardNavigationGestures
+        self.scriptHandler = scriptHandler
         self.onUpdate = onUpdate
     }
 
     public func makeUIView(context: Context) -> WKWebView {
-        let webView = WKWebView(frame: .zero, configuration: configuration)
+        let webView = WebViewRecycler.shared.dequeue()
+        webView.setCookies(cookies)
         webView.navigationDelegate = context.coordinator
         webView.allowsBackForwardNavigationGestures = allowsBackForwardNavigationGestures
+        if let scriptHandler {
+            webView.configuration.userContentController.add(context.coordinator, name: scriptHandler.name)
+        }
         onUpdate?(webView)
         let request = URLRequest(url: url)
         webView.load(request)
@@ -46,11 +47,21 @@ public struct SwiftUIWebView: UIViewRepresentable {
         onUpdate?(webView)
     }
 
+    public static func dismantleUIView(_ webView: WKWebView, coordinator: Coordinator) {
+        guard let recyclableWebView = webView as? RecyclableWebView else { return }
+        WebViewRecycler.shared.enqueue(recyclableWebView)
+    }
+
     public func makeCoordinator() -> Coordinator {
-        Coordinator()
+        Coordinator(scriptHandler: scriptHandler)
     }
 
     public class Coordinator: NSObject, WKNavigationDelegate {
+        private let scriptHandler: ScriptMessageHandler?
+        init(scriptHandler: ScriptMessageHandler?) {
+            self.scriptHandler = scriptHandler
+        }
+
         public func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
         }
 
@@ -58,6 +69,25 @@ public struct SwiftUIWebView: UIViewRepresentable {
         }
 
         public func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: any Error) {
+        }
+    }
+}
+
+public struct ScriptMessageHandler: Sendable {
+    public let name: String
+    public let handler: @Sendable (String) -> Void
+
+    public init(name: String, handler: @Sendable @escaping (String) -> Void) {
+        self.name = name
+        self.handler = handler
+    }
+}
+
+extension SwiftUIWebView.Coordinator: WKScriptMessageHandler {
+    public func userContentController(_: WKUserContentController, didReceive message: WKScriptMessage) {
+        assert(message.name == scriptHandler?.name)
+        if let body = message.body as? String {
+            scriptHandler?.handler(body)
         }
     }
 }
