cb44ef32e37641ac227fc559a52dc645dc8aee39
shinhong_park <shp7724@snu.ac.kr>
2025-10-23 22:25:05 +0900

    📊 Analytics 모듈 추가 및 의존성 통합

---
+++ SNUTT/Modules/Feature/Analytics/Sources/FirebaseAnalyticsLogger.swift
@@ -0,0 +1,37 @@
+//
+//  FirebaseAnalyticsLogger.swift
+//  SNUTT
+//
+//  Copyright © 2025 wafflestudio.com. All rights reserved.
+//
+
+import AnalyticsInterface
+import Dependencies
+import os
+
+public struct FirebaseAnalyticsLogger: AnalyticsLogger {
+    @Dependency(\.analyticsSDK) private var analytics
+
+    public init() {}
+
+    public func logEvent(_ event: any AnalyticsLogEvent) {
+        analytics.logEvent(event.eventName, parameters: event.parameters)
+    }
+
+    public func logScreen(_ screen: any AnalyticsLogEvent) {
+        var parameters = [String: String]()
+        parameters[Constants.screenName] = screen.eventName
+        parameters[Constants.screenClass] = screen.eventName
+        analytics.logEvent(Constants.screenView, parameters: parameters)
+
+        if let extraParameters = screen.parameters {
+            analytics.logEvent(screen.eventName, parameters: extraParameters)
+        }
+    }
+
+    private enum Constants {
+        static let screenName = "screen_name"
+        static let screenClass = "screen_class"
+        static let screenView = "screen_view"
+    }
+}
