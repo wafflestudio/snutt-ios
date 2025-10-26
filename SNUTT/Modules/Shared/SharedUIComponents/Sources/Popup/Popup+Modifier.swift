12b21b35f0736c77f4d2f7a148476d593b7f6567
shinhong_park <shp7724@snu.ac.kr>
2025-10-27 00:39:25 +0900

    ✨ 공통 팝업 컴포넌트 구현

---
+++ SNUTT/Modules/Shared/SharedUIComponents/Sources/Popup/Popup+Modifier.swift
@@ -0,0 +1,35 @@
+//
+//  Popup+Modifier.swift
+//  SNUTT
+//
+//  Copyright © 2025 wafflestudio.com. All rights reserved.
+//
+
+import SwiftUI
+
+private typealias PopupPresentationContext = HUDPresentationContext<Popup<AnyView>>
+private typealias PopupPresentationKey = HUDPresentationKey<Popup<AnyView>>
+
+extension View {
+    public func overlayPopup() -> some View {
+        modifier(HUDPresentationModifier<Popup<AnyView>>())
+    }
+
+    public func customPopup<Content: View>(
+        isPresented: Binding<Bool>,
+        @ViewBuilder content: @MainActor @escaping () -> Content
+    ) -> some View {
+        preference(
+            key: PopupPresentationKey.self,
+            value: .init(
+                id: .init(),
+                isPresented: isPresented,
+                content: {
+                    Popup(isPresented: isPresented) {
+                        AnyView(content())
+                    }
+                }
+            )
+        )
+    }
+}
