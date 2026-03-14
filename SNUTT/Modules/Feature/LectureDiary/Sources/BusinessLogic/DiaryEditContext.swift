95db38d63f00e4d3d40787ee823c7b06d543f27e
Yoorim Choi <penguin0807@snu.ac.kr>
2026-03-14 18:33:25 +0900

    fix typo

--- SNUTT/Modules/Feature/LectureDiary/Sources/BusinessLogic/DiaryEditContext.swift
+++ SNUTT/Modules/Feature/LectureDiary/Sources/BusinessLogic/DiaryEditContext.swift
@@ -8,7 +8,7 @@
 
     import Foundation
 
-    public struct DiaryEditContext: Sendable, Identifiable {
+    public struct DiaryEditContext: Sendable, Identifiable, Equatable {
 
         public let id = UUID()
         public let lectureID: String
