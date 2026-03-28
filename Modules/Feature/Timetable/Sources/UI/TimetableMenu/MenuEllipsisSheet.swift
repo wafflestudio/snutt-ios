2f34b1e0e92747c44ecd6df358b9f77e61ad2229
shinhong-park <shp7724@snu.ac.kr>
2026-03-28 21:55:13 +0900

    refactor: migrate MenuEllipsisSheet to ActionSheet abstraction

--- Modules/Feature/Timetable/Sources/UI/TimetableMenu/MenuEllipsisSheet.swift
+++ Modules/Feature/Timetable/Sources/UI/TimetableMenu/MenuEllipsisSheet.swift
@@ -28,54 +28,47 @@ struct MenuEllipsisSheet: View {
     @State private var timetableImage: TimetableImage?
 
     var body: some View {
-        GeometryReader { _ in
-            VStack {
-                EllipsisSheetButton(menu: .edit) {
-                    isRenameMenuPresented = true
-                }
-                .sheet(isPresented: $isRenameMenuPresented) {
-                    MenuRenameSheet(viewModel: viewModel, metadata: metadata)
-                }
-
-                EllipsisSheetButton(menu: .primary(isOn: metadata.isPrimary)) {
-                    dismiss()
-                    errorAlertHandler.withAlert {
-                        if metadata.isPrimary {
-                            try await viewModel.unsetPrimaryTimetable(timetableID: metadata.id)
-                        } else {
-                            try await viewModel.setPrimaryTimetable(timetableID: metadata.id)
-                        }
-                    }
-                }
-
-                EllipsisSheetButton(menu: .share) {
-                    errorAlertHandler.withAlert {
-                        timetableImage = try await viewModel.createTimetableImage(
-                            timetable: metadata,
-                            colorScheme: colorScheme,
-                            availableThemes: themeViewModel.availableThemes
-                        )
-                    }
-                }
-
-                EllipsisSheetButton(menu: .theme) {
-                    menuSheetDismiss()
-                    viewModel.presentThemeSheet()
-                }
-
-                EllipsisSheetButton(menu: .delete) {
-                    dismiss()
-                    Task {
-                        errorAlertHandler.withAlert {
-                            try await viewModel.deleteTimetable(timetableID: metadata.id)
-                        }
-                    }
-                }
-                Spacer()
+        ActionSheet {
+            ActionSheetItem(image: Menu.edit.image, title: Menu.edit.text) {
+                isRenameMenuPresented = true
             }
-            .padding(.vertical, 10)
-            .padding(.horizontal, 5)
-            .presentationDetents([.height(260)])
+            ActionSheetItem(
+                image: Menu.primary(isOn: metadata.isPrimary).image,
+                title: Menu.primary(isOn: metadata.isPrimary).text
+            ) {
+                dismiss()
+                errorAlertHandler.withAlert {
+                    if metadata.isPrimary {
+                        try await viewModel.unsetPrimaryTimetable(timetableID: metadata.id)
+                    } else {
+                        try await viewModel.setPrimaryTimetable(timetableID: metadata.id)
+                    }
+                }
+            }
+            ActionSheetItem(image: Menu.share.image, title: Menu.share.text) {
+                errorAlertHandler.withAlert {
+                    timetableImage = try await viewModel.createTimetableImage(
+                        timetable: metadata,
+                        colorScheme: colorScheme,
+                        availableThemes: themeViewModel.availableThemes
+                    )
+                }
+            }
+            ActionSheetItem(image: Menu.theme.image, title: Menu.theme.text) {
+                menuSheetDismiss()
+                viewModel.presentThemeSheet()
+            }
+            ActionSheetItem(image: Menu.delete.image, title: Menu.delete.text) {
+                dismiss()
+                Task {
+                    errorAlertHandler.withAlert {
+                        try await viewModel.deleteTimetable(timetableID: metadata.id)
+                    }
+                }
+            }
+        }
+        .sheet(isPresented: $isRenameMenuPresented) {
+            MenuRenameSheet(viewModel: viewModel, metadata: metadata)
         }
         .sheet(item: $timetableImage) { image in
             TimetableShareSheet(timetableImage: image)
@@ -84,57 +77,32 @@ struct MenuEllipsisSheet: View {
     }
 }
 
-private struct EllipsisSheetButton: View {
-    let menu: Menu
-    let action: () -> Void
+private enum Menu {
+    case edit
+    case primary(isOn: Bool)
+    case share
+    case theme
+    case delete
 
-    var body: some View {
-        AnimatableButton(
-            animationOptions: .backgroundColor(touchDown: .label.opacity(0.05)).scale(0.99),
-            layoutOptions: [.expandHorizontally, .respectIntrinsicHeight]
-        ) {
-            action()
-        } configuration: { button in
-            var config = UIButton.Configuration.plain()
-            config.image = menu.image
-            config.title = menu.text
-            config.imagePadding = 10
-            config.baseForegroundColor = .label
-            button.contentHorizontalAlignment = .leading
-            config.background.cornerRadius = 10
-            return config
+    var image: Image {
+        switch self {
+        case .edit: Image(uiImage: TimetableAsset.sheetEdit.image)
+        case .primary(true): Image(uiImage: TimetableAsset.sheetFriendOff.image)
+        case .primary(false): Image(uiImage: TimetableAsset.sheetFriend.image)
+        case .share: Image(uiImage: TimetableAsset.navShare.image)
+        case .theme: Image(uiImage: TimetableAsset.sheetPalette.image)
+        case .delete: Image(uiImage: TimetableAsset.sheetTrash.image)
         }
     }
-}
 
-extension EllipsisSheetButton {
-    enum Menu {
-        case edit
-        case primary(isOn: Bool)
-        case share
-        case theme
-        case delete
-
-        var image: UIImage {
-            switch self {
-            case .edit: return TimetableAsset.sheetEdit.image
-            case .primary(true): return TimetableAsset.sheetFriendOff.image
-            case .primary(false): return TimetableAsset.sheetFriend.image
-            case .share: return TimetableAsset.navShare.image
-            case .theme: return TimetableAsset.sheetPalette.image
-            case .delete: return TimetableAsset.sheetTrash.image
-            }
-        }
-
-        var text: String {
-            switch self {
-            case .edit: return TimetableStrings.sheetEllipsisEditTitle
-            case .primary(true): return TimetableStrings.sheetEllipsisPrimaryDisable
-            case .primary(false): return TimetableStrings.sheetEllipsisPrimaryEnable
-            case .share: return TimetableStrings.sheetEllipsisShare
-            case .theme: return TimetableStrings.sheetEllipsisTheme
-            case .delete: return TimetableStrings.sheetEllipsisDelete
-            }
+    var text: String {
+        switch self {
+        case .edit: TimetableStrings.sheetEllipsisEditTitle
+        case .primary(true): TimetableStrings.sheetEllipsisPrimaryDisable
+        case .primary(false): TimetableStrings.sheetEllipsisPrimaryEnable
+        case .share: TimetableStrings.sheetEllipsisShare
+        case .theme: TimetableStrings.sheetEllipsisTheme
+        case .delete: TimetableStrings.sheetEllipsisDelete
         }
     }
 }
