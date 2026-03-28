4b9066d8716798ce0cdc20a922a5f7ba467c1a58
shinhong-park <shp7724@snu.ac.kr>
2026-03-28 21:52:34 +0900

    feat: add ActionSheet view with automatic presentationDetents

---
+++ Modules/Shared/SharedUIComponents/Sources/ActionSheet/ActionSheet.swift
@@ -0,0 +1,37 @@
+//
+//  ActionSheet.swift
+//  SNUTT
+//
+//  Copyright © 2026 wafflestudio.com. All rights reserved.
+//
+
+import SwiftUI
+
+public struct ActionSheet: View {
+    private let items: [ActionSheetItem]
+
+    public init(@ActionSheetBuilder items: () -> [ActionSheetItem]) {
+        self.items = items()
+    }
+
+    public var body: some View {
+        VStack(spacing: 0) {
+            VStack(spacing: 0) {
+                ForEach(items) { item in
+                    Button {
+                        item.action()
+                    } label: {
+                        ActionSheetLabel(image: item.image, title: item.title, role: item.role)
+                    }
+                }
+            }
+            .buttonStyle(ActionSheetButtonStyle())
+            .clipShape(ContainerRelativeShape())
+
+            Spacer()
+        }
+        .padding(.vertical, 10)
+        .padding(.horizontal, 5)
+        .presentationDetents([.height(ActionSheetLabel.rowHeight * CGFloat(items.count) + 20)])
+    }
+}
